869ddfa49772dab15fee7c9aac5c9f3a73f850ed
Ivan Kraev <ivankraev2@abv.bg>
2022-03-18 10:13:51 +0200

    resolve conflicts

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,10 @@
-<<<<<<< HEAD
-import express from "express";
-import dotenv from "dotenv";
-import routes from "./routes";
-import logger from "./utils/logger";
-import { deserializeUser } from "./middleware/deserializeUser";
-dotenv.config();
-=======
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
->>>>>>> main
 
 const app = express()
 const port = process.env.API_PORT || 3000
 
-<<<<<<< HEAD
-app.use(express.json());
-app.use(deserializeUser);
-
-app.listen(port, () => {
-  logger.info(`App is running at http://localhost:${port}`);
-  routes(app);
-});
-=======
 app.use(express.json())
 
 app.listen(port, () => {
@@ -31,4 +13,3 @@ app.listen(port, () => {
 app.get('/healthcheck', (req, res) => {
   res.send('<h1>Hello</h1>')
 })
->>>>>>> main
