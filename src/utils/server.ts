b99b84268c0cf5a30d70bfcdb656d6d405fdbfb8
Ivan Kraev <ivankraev2@abv.bg>
2022-03-24 10:32:56 +0200

    added cors

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import { deserializeUser } from '../middleware/deserializeUser'
+const corsOptions = {
+  origin: 'http://localhost:3000',
+}
 dotenv.config()
 import routes from '../routes'
 function createServer() {
   const app = express()
   app.use(express.json())
+  app.use(cors(corsOptions))
   /**
    * * Check if the user provides valid token
    * ? If not
